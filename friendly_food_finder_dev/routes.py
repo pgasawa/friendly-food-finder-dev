dc70c9a39b59a6a73b4ab9370ff74dab99e910a4
arvindrajaraman <arvind6902@gmail.com>
2023-10-27 22:07:02 -0700

    added routes

---
+++ friendly_food_finder_dev/routes.py
@@ -0,0 +1,43 @@
+import reflex as rx
+
+async def user_create():
+    raise NotImplementedError
+
+async def user_signin():
+    raise NotImplementedError
+
+async def user_setdietaryprefs():
+    raise NotImplementedError
+
+async def user_setcuisineprefs():
+    raise NotImplementedError
+
+async def user_addfriend():
+    raise NotImplementedError
+
+async def user_showallfriends():
+    raise NotImplementedError
+
+async def user_showavailablefriends():
+    raise NotImplementedError
+
+async def hangout_getfriendclusters():
+    raise NotImplementedError
+
+async def hangout_getrestaurants():
+    raise NotImplementedError
+
+async def hangout_invitecreate():
+    raise NotImplementedError
+
+async def hangout_inviteaccept():
+    raise NotImplementedError
+
+async def hangout_invitedecline():
+    raise NotImplementedError
+
+async def feed_show():
+    raise NotImplementedError
+
+async def history_show():
+    raise NotImplementedError
