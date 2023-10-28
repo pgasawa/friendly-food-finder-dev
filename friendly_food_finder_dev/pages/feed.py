5ab57b34be141afef421af2537ced6e64cbbf010
arvindrajaraman <arvind6902@gmail.com>
2023-10-27 23:46:23 -0700

    commented out feed.pty

--- friendly_food_finder_dev/pages/feed.py
+++ friendly_food_finder_dev/pages/feed.py
@@ -13,25 +13,28 @@ def feed() -> rx.Component:
     Returns:
         The UI for the feed page.
     """
+    # return rx.vstack(
+    #     eventComponent(),
+    #     rx.center(
+    #         rx.box(
+    #             rx.heading("Feed", font_size="3em"),
+    #             rx.text("Welcome to Feed!"),
+    #             **styles.template_content_style,
+    #         ),
+    #     ),
+    #     rx.text("Testing what happens rn"),
+    # )
     return rx.vstack(
-        eventComponent(),
-        rx.center(
-            rx.box(
-                rx.heading("Feed", font_size="3em"),
-                rx.text("Welcome to Feed!"),
-                **styles.template_content_style,
-            ),
-        ),
-        rx.text("Testing what happens rn"),
+        rx.text("Testing what happens rn")
     )
 
-def eventComponent() -> rx.Component:
-    firestore_client = Firebase.get_firestore_client()
-    events = firestore_client.get_all_documents_from_collection()
-    for event in events:
-        print(event)
-    return rx.card(
-    rx.text("Body of the Card Component"),
-    header=rx.heading("Header", size="lg"),
-    footer=rx.heading("Footer", size="sm"),
-)
+# def eventComponent() -> rx.Component:
+#     firestore_client = Firebase.get_firestore_client()
+#     events = firestore_client.get_all_documents_from_collection()
+#     for event in events:
+#         print(event)
+#     return rx.card(
+#     rx.text("Body of the Card Component"),
+#     header=rx.heading("Header", size="lg"),
+#     footer=rx.heading("Footer", size="sm"),
+# )
